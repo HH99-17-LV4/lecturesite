d402b0e8b98deec6b3b48e6bb67b642314e8a4eb
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-15 20:01:16 +0900

    init commit, gitignore 에 property file 추가

---
+++ src/main/java/com/leeseo/lecturesite/LecturesiteApplication.java
@@ -0,0 +1,13 @@
+package com.leeseo.lecturesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecturesiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LecturesiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leeseo/lecturesite/LecturesiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeseo.lecturesite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecturesiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
