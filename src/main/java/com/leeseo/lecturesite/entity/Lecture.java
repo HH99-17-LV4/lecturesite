cf0f1cd7306d37eccaa5b4e4a5ffec230e6ae253
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-16 20:40:13 +0900

    lecture entity에서 좋아요 수 제거

--- src/main/java/com/leeseo/lecturesite/entity/Lecture.java
+++ src/main/java/com/leeseo/lecturesite/entity/Lecture.java
@@ -1,12 +1,18 @@
 package com.leeseo.lecturesite.entity;
 
-import com.leeseo.lecturesite.dto.LectureRequestDto;
-import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
+import com.leeseo.lecturesite.dto.LectureRequestDto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
@@ -14,24 +20,22 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
 public class Lecture extends TimeStamped {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Integer price;
+	private String comment;
+	private String category;
+	@ManyToOne
+	private Tutor tutor;
 
-    private String name;
-    private Integer price;
-    private String  comment;
-    private String category;
-    @ManyToOne
-    private Tutor tutor;
-    private int likes;
+	public Lecture(LectureRequestDto requestDto) {
+		this.name = requestDto.getName();
+		this.price = requestDto.getPrice();
+		this.comment = requestDto.getComment();
+		this.category = requestDto.getCategory();
 
-    public Lecture(LectureRequestDto requestDto) {
-        this.name = requestDto.getName();
-        this.price = requestDto.getPrice();
-        this.comment = requestDto.getComment();
-        this.category = requestDto.getCategory();
-
-    }
+	}
 
 }
