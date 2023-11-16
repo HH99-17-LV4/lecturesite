f42c82d1d45f6dd27c067cc5fa682392f6a3df2c
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-16 21:32:34 +0900

    ResponseDto에 Tutor -> tutorName으로 수정

--- src/main/java/com/leeseo/lecturesite/dto/LectureResponseDto.java
+++ src/main/java/com/leeseo/lecturesite/dto/LectureResponseDto.java
@@ -12,14 +12,19 @@ public class LectureResponseDto {
     private Integer price;
     private String category;
     private String comment;
-    private Tutor tutor;
+    private String tutorName; // 연관관계에 있는 엔티티를 직접 추가하지않도록 유의
     private LocalDateTime registeredAt;
+    private Integer likes;
 
     public LectureResponseDto(Lecture lecture) {
         this.name = lecture.getName();
         this.price = lecture.getPrice();
         this.category = lecture.getCategory();
         this.comment = lecture.getComment();
+        this.tutorName = lecture.getTutor().getName();
+        this.likes = lecture.getLikes().size();
         this.registeredAt = lecture.getRegisteredAt();
     }
+
+
 }
