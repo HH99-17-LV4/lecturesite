a4589cd07995477b49e742a680777985add3ccaa
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-16 16:20:23 +0900

    생성자 추가

--- src/main/java/com/leeseo/lecturesite/dto/user/LoginRequestDto.java
+++ src/main/java/com/leeseo/lecturesite/dto/user/LoginRequestDto.java
@@ -1,10 +1,16 @@
 package com.leeseo.lecturesite.dto.user;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequestDto {
     private String username;
     private String password;
